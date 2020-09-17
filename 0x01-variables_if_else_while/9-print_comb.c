11c00aadeeac91bf734cf04f66df6a9050bc5ac8
Gabriel Prochnau <2288@holbertonschool.com>
2020-09-17 21:04:59 +0000

    Fix part 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,15 +7,15 @@
 int main(void)
 {
 int a;
-for(a = '0'; a <= '9'; a++)
+for (a = '0'; a <= '9'; a++)
 {
 putchar(a);
-if(a != '9')
+if (a != '9')
 {
 putchar(',');
 putchar(' ');
 }
 }
 putchar('\n');
-return(0);
+return (0);
 }
