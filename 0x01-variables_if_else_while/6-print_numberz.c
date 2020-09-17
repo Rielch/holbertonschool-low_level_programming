c8ab5bfdac55fa6249414e85bb71cddce75d3f16
Gabriel Prochnau <2288@holbertonschool.com>
2020-09-17 19:52:31 +0000

    Add part 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+int n = *"\n";
+for (a = 0; a <= 9; a++)
+putchar((a % 10) + '0');
+putchar(n);
+return (0);
+}
