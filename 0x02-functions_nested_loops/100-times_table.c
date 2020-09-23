b9043f9841f9db7f575cd50a10b3288e5b8a82b8
Gabriel Prochnau <2288@holbertonschool.com>
2020-09-23 00:29:04 +0000

    Fix part 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,27 +7,19 @@
 void print_times_table(int n)
 {
 int a, b, c, d, e, f;
-if (n < 15 && n > 0)
+if (n <= 15 && n >= 0)
 {
 for (a = 0; a <= n; a++)
 {
 for (b = 0; b <= n; b++)
 {
 c = a * b;
-f = c / 100;
-d = (c / 10) % 10;
-e = c % 10;
+d = c / 100;
+e = (c / 10) % 10;
+f = c % 10;
 if (b != 0)
 {
-if (f != 0)
-{
-_putchar(f + '0');
-}
-else
-{
-_putchar(' ');
-}
-if ((d != 0 && f == 0) || f != 0)
+if (d != 0)
 {
 _putchar(d + '0');
 }
@@ -35,8 +27,16 @@ else
 {
 _putchar(' ');
 }
-}
+if ((e != 0 && d == 0) || d != 0)
+{
 _putchar(e + '0');
+}
+else
+{
+_putchar(' ');
+}
+}
+_putchar(f + '0');
 if (b != n)
 {
 _putchar(',');
