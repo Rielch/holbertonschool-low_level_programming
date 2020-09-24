32da87192b16c6dc110c2796a817239c32ea884b
Gabriel Prochnau <2288@holbertonschool.com>
2020-09-24 20:57:28 +0000

    Add part 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,31 +6,31 @@
 
 int main(void)
 {
-  long a = 612852475143;
-  int flag;
-  long b, c;
-  for (b = 2; b <= a; b++)
-    {
-      if (a % b == 0)
-	{
-	  flag = 0;
-	  for (c = 2; c <= (b / 2); c++)
-	    {
-	      if (b % c == 0)
-		{
-		  flag = 1;
-		  break;
-		}
-	    }
-	  if (flag == 0)
-	    {
-	      if ((a / b) == 1)
-		{
-		  printf("%ld\n", a);
-		}
-	      a = a / b;
-	    }
-	}
-    }
-  return (0);
+long a = 612852475143;
+int flag;
+long b, c;
+for (b = 2; b <= a; b++)
+{
+if (a % b == 0)
+{
+flag = 0;
+for (c = 2; c <= (b / 2); c++)
+{
+if (b % c == 0)
+{
+flag = 1;
+break;
+}
+}
+if (flag == 0)
+{
+if ((a / b) == 1)
+{
+printf("%ld\n", a);
+}
+a = a / b;
+}
+}
+}
+return (0);
 }
