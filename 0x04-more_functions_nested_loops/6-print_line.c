6691bb3f77bb15e85fbc32bfa9d12f329eb693a8
Gabriel Prochnau <2288@holbertonschool.com>
2020-09-24 16:44:20 +0000

    Fix part 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,6 @@
 /**
  * print_line - Entry point
  * @n: number of '_' to print
- * Return: Always 0 (Success)
  */
 
 void print_line(int n)
@@ -10,7 +9,7 @@ void print_line(int n)
 int m = 0;
 if (n > 0)
 {
-while (m <= n)
+while (m < n)
 {
 _putchar('_');
 m++;
