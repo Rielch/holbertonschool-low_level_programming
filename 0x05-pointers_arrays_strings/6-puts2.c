d329a117e0cfd14bc3aee9636cf75f74b6e59254
Gabriel Prochnau <2288@holbertonschool.com>
2020-09-29 12:50:38 +0000

    Add part 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints odd characters
+ *
+ * @str: string
+ * Return: Void.
+ */
+
+void puts2(char *str)
+{
+int a;
+ for (a = 0; *(str + a) != '\0'; a++)
+   {
+     if (a % 2 == 0)
+       {
+	 _putchar(str[a]);
+       }
+   }
+ _putchar('\n');
+}
