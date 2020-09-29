c788ae3ab988f783445e1c1e1672f1fd78571741
Gabriel Prochnau <2288@holbertonschool.com>
2020-09-29 15:57:27 +0000

    Add part 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - copy a string from one buffer to another
+ *
+ * @src: source string
+ * @dest: destiny buffer
+ * Return: Pointer to dest.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+char *point = dest;
+for (; *src != '\0'; src++)
+{
+*dest = *src;
+dest++;
+}
+*dest = '\0';
+return point;
+}
