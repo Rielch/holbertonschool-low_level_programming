8f29e6040aed7e861be93c6e58ebd8c54ab92dd3
Gabriel Prochnau <2288@holbertonschool.com>
2020-09-29 12:51:40 +0000

    Fix Betty style in 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,12 +10,12 @@
 void puts2(char *str)
 {
 int a;
- for (a = 0; *(str + a) != '\0'; a++)
-   {
-     if (a % 2 == 0)
-       {
-	 _putchar(str[a]);
-       }
-   }
- _putchar('\n');
+for (a = 0; *(str + a) != '\0'; a++)
+{
+if (a % 2 == 0)
+{
+_putchar(str[a]);
+}
+}
+_putchar('\n');
 }
