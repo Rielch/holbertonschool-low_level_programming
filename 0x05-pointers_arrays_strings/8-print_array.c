0e72d7c00ecf31f07dc9d4820386c7f359ba9453
Gabriel Prochnau <2288@holbertonschool.com>
2020-09-29 13:13:56 +0000

    Add Betty style to 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,14 +11,14 @@
 
 void print_array(int *a, int n)
 {
-  int b;
-  for (b = 0; b < n; b++)
-    {
-      printf("%d", *(a + b));
-      if (b < (n - 1))
-	{
-	  printf(", ");
-	}
-    }
-  putchar('\n');
+int b;
+for (b = 0; b < n; b++)
+{
+printf("%d", *(a + b));
+if (b < (n - 1))
+{
+printf(", ");
+}
+}
+putchar('\n');
 }
