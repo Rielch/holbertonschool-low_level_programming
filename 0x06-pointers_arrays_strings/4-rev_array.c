a1dcae76f95bdf9b46814cdee04abad731b5a2e5
Gabriel Prochnau <2288@holbertonschool.com>
2020-10-01 18:42:50 +0000

    Add part 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,5 +10,11 @@
 
 void reverse_array(int *a, int n)
 {
-int
+  int b, temp;
+  for (b = 0; b < ((n - 1) / 2); b++)
+    {
+      temp = a[b];
+      a[b] = a[n - 1 - b];
+      a[n - 1 - b] = temp;
+    }
 }
