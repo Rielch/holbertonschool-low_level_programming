8ddc9407623128d6882c0b79564b4889ec6fd87e
Gabriel Prochnau <2288@holbertonschool.com>
2020-10-01 18:43:54 +0000

    Fix Betty style for 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,11 +10,11 @@
 
 void reverse_array(int *a, int n)
 {
-  int b, temp;
-  for (b = 0; b < ((n - 1) / 2); b++)
-    {
-      temp = a[b];
-      a[b] = a[n - 1 - b];
-      a[n - 1 - b] = temp;
-    }
+int b, temp;
+for (b = 0; b < ((n - 1) / 2); b++)
+{
+temp = a[b];
+a[b] = a[n - 1 - b];
+a[n - 1 - b] = temp;
+}
 }
