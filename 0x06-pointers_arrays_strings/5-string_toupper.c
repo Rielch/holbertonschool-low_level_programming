fb6e7c9a223ff4c9ffed334897c5a4d040fe2656
Gabriel Prochnau <2288@holbertonschool.com>
2020-10-01 18:55:35 +0000

    Add part 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - change all lower case letters to upper case.
+ *
+ * @s: string
+ * Return: String in upper case.
+ */
+
+char *string_toupper(char *s)
+{
+int a;
+for (a = 0; s[a] != '\0'; a++)
+{
+if (s[a] <= 'z' && s[a] >= 'a')
+{
+s[a] = s[a] - 32;
+}
+}
+return (s);
+}
