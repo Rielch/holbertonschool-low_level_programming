9fd375bb4e56528d3beabfef77c83132e825f31d
Gabriel Prochnau <2288@holbertonschool.com>
2020-10-02 19:22:38 +0000

    Add 100

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -9,5 +9,22 @@
 
 void print_number(int n)
 {
-
+  if (n < 0)
+    {
+      _putchar('-');
+      n = n * -1;
+    }
+  if (n > 999)
+    {
+      _putchar((n / 1000) % 10);
+    }
+  if (n > 99)
+    {
+      _putchar((n / 100) % 10);
+    }
+  if (n > 9)
+    {
+      _putchar((n / 10) % 10);
+    }
+  _putchar(n % 10);
 }
