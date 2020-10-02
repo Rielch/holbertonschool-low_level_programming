c94b91d28c3b841018e697bcb83ebaa6dcaf6fe0
Gabriel Prochnau <2288@holbertonschool.com>
2020-10-02 18:12:10 +0000

    Add advanced from 0x05

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -23,10 +23,6 @@ int _atoi(char *s)
   for (; s[a] >= '0' && s[a] <= '9'; a++)
     {
       res = (res * 10) + s[a];
-      if (a == size)
-	{
-	  return (res);
-	}
     }
   for (b = 0; b < a; b++)
     {
---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * print_number - prints an integer
+ *
+ * @n: number.
+ * Return: Void.
+ */
+
+void print_number(int n)
+{
+
+}
