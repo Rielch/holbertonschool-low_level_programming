d0313856dc43f29f259ff03f8b7704acfb1e05ee
Gabriel Prochnau <2288@holbertonschool.com>
2020-10-02 13:51:01 +0000

    Add part 101

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - creates valid password for 101-crackme
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+  int a;
+  char res[100];
+  char last;
+  int sum;
+  srand(time(NULL));
+  for (a = 0; sum < 2645; a++)
+    {
+      res[a] = (rand() % 26 + 97);
+      sum = sum + res[a];
+      putchar(res[a]);
+    }
+  res[a + 1] = 2772 - sum;
+  putchar(res[a + 1]);
+  return (0);
+}
