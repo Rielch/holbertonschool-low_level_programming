9a70b9d2955acb463662616dba58a354adc5f391
Gabriel Prochnau <2288@holbertonschool.com>
2020-10-05 21:46:19 +0000

    Add betty style to part 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,17 +9,17 @@
 
 void print_chessboard(char (*a)[8])
 {
-  int b, c;
+int b, c;
 
-  for (b = 0; b < 8; b++)
-    {
-      for (c = 0; c < 8; c++)
-	{
-	  _putchar(a[b][c]);
-	  if (c == 7)
-	    {
-	      _putchar('\n');
-	    }
-	}
-    }
+for (b = 0; b < 8; b++)
+{
+for (c = 0; c < 8; c++)
+{
+_putchar(a[b][c]);
+if (c == 7)
+{
+_putchar('\n');
+}
+}
+}
 }
