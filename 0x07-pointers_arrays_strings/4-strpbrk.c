725468d1d91972ff2878a1bc245ae79ffccdeda6
Gabriel Prochnau <2288@holbertonschool.com>
2020-10-05 15:27:12 +0000

    asd

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ *
+ * @s: string of text
+ * @accept: set of bytes
+ * Return: pointer to byte in s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+  int a, b;
+  for (a = 0; s[a] != '\0'; a++)
+    {
+      for (b = 0; accept[b] != '\0'; b++)
+	{
+	  if (s[a] == accept[b])
+	    {
+	      return (&s[a]);
+	    }
+	}
+    }
+  return (NULL);
+}
