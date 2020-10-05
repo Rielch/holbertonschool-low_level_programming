85d407fdd5e88f50e6394fc5593c9033ee478e75
Gabriel Prochnau <2288@holbertonschool.com>
2020-10-05 18:09:19 +0000

    Fix betty style for part 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,16 +10,16 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-  int a, b;
-  for (a = 0; s[a] != '\0'; a++)
-    {
-      for (b = 0; accept[b] != '\0'; b++)
-	{
-	  if (s[a] == accept[b])
-	    {
-	      return (&s[a]);
-	    }
-	}
-    }
-  return ('\0');
+int a, b;
+for (a = 0; s[a] != '\0'; a++)
+{
+for (b = 0; accept[b] != '\0'; b++)
+{
+if (s[a] == accept[b])
+{
+return (&s[a]);
+}
+}
+}
+return ('\0');
 }
