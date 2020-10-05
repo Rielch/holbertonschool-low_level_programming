fc0be43c2276057eb940a29a856dff5f28015c00
Gabriel Prochnau <2288@holbertonschool.com>
2020-10-05 21:45:08 +0000

    Add part 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - prints a chessboard
+ *
+ * @a: content of the chessboard
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+  int b, c;
+
+  for (b = 0; b < 8; b++)
+    {
+      for (c = 0; c < 8; c++)
+	{
+	  _putchar(a[b][c]);
+	  if (c == 7)
+	    {
+	      _putchar('\n');
+	    }
+	}
+    }
+}
