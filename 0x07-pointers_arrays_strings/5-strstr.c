6a143dbf5ca0d6ea9a347ca45ea0fe266009bd7e
Gabriel Prochnau <2288@holbertonschool.com>
2020-10-05 19:19:51 +0000

    Add part 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * _strstr - locates a substing
+ *
+ * @haystack: string
+ * @needle: string to be searched
+ * Return: pointer to the beginning of @needle.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+int a, b;
+for (a = 0; haystack[a] != '\0'; a++)
+{
+for (b = 0; needle[b] != '\0'; b++)
+{
+if (haystack[a + b] == needle[b])
+{
+if (needle[b + 1] == '\0')
+{
+return (&haystack[a]);
+}
+continue;
+}
+else
+{
+break;
+}
+}
+}
+return ('\0');
+}
