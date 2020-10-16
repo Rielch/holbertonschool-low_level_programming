5996e91dd72690d892fa0204d3df89c0d5e3d8ef
Gabriel Prochnau <2288@holbertonschool.com>
2020-10-16 01:08:44 +0000

    Add part 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,57 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ *
+ * @s1: first string
+ * @s2: seconde string
+ * @n: fisrt bytes of s2 to be copied
+ * Return: pointer to new string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+  unsigned int size1, size2, str2, a;
+  char *str;
+  if (s1 == NULL)
+    {
+      s1 = "";
+    }
+  if (s2 == NULL)
+    {
+      s2 = "";
+    }
+  for (size1 = 0; s1[size1] != '\0'; size1++)
+    {
+    }
+  for (size2 = 0; s2[size2] != '\0'; size2++)
+    {
+    }
+  if (size2 >= n)
+    {
+      str2 = n;
+    }
+  else
+    {
+      str2 = size2;
+    }
+  str = malloc((size1 + 1 + str2) * sizeof(char));
+  if (str == NULL)
+    {
+      return ('\0');
+    }
+  for (a = 0; a < (size1 + str2); a++)
+    {
+      if (a < size1)
+	{
+	  str[a] = s1[a];
+	}
+      else
+	{
+	  str[a] = s2[a - size1];
+	}
+    }
+  str[a + 1] = '\0';
+  return (str);
+}
