eae1910d2b3cc0afa8dd5599fe5b8d34425e1d0e
Gabriel Prochnau <2288@holbertonschool.com>
2020-10-19 18:21:53 +0000

    Add part 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,18 @@
 #include "dog.h"
 
 /**
- * init_dog - 
+ * init_dog - initialize a variable
+ *
+ * @d: struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: Void.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
