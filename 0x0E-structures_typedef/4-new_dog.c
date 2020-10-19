a3b2bdb89fcdbeb8529d62bb7be7f65514e67d89
Gabriel Prochnau <2288@holbertonschool.com>
2020-10-19 22:05:53 +0000

    Fix part 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,14 +13,51 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *ndog;
+ char *name2;
+ char *owner2;
 ndog = malloc(sizeof(dog_t));
 if (ndog == NULL)
 {
 free(ndog);
 return (NULL);
 }
-ndog->name = name;
-ndog->age = age;
-ndog->owner = owner;
+ ndog->name = name;
+ ndog->age = age;
+ ndog->owner = owner;
+ name2 = malloc(sizeof(*name));
+ if (name2 == NULL)
+   {
+     free(name2);
+     free(ndog);
+     return (NULL);
+   }
+ owner2 = malloc(sizeof(*owner));
+ if (owner2 == NULL)
+   {
+     free(name2);
+     free(owner2);
+     free(ndog);
+     return (NULL);
+   }
+ if (name != NULL)
+   {
+     while (*name != '\0')
+       {
+	 *name2 = *name;
+	 name++;
+	 name2++;
+       }
+     *(name2 + 1) = '\0';
+   }
+ if (owner != NULL)
+   {
+     while (*owner != '\0')
+       {
+	 *owner2 = *owner;
+	 owner++;
+	 owner2++;
+       }
+     *(owner2 + 1) = '\0';
+   }
 return (ndog);
 }
