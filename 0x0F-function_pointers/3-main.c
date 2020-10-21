2bfc5107bb3c4acb04311685742ab53c598124b7
Gabriel Prochnau <2288@holbertonschool.com>
2020-10-21 21:40:01 +0000

    Add part 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * main - performs simple operations between two numbers
+ *
+ * @argc: size of argv
+ * @argv: arguments taken for the program
+ * Return: always 0
+ */
+
+int main(int argc, char **argv)
+{
+int a;
+int b;
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+if (get_op_func(argv[2]) == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+printf("%d\n", get_op_func(argv[2])(a, b));
+return (0);
+}
