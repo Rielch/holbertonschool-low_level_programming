a9bb0c92decc0c0c486de290109731ded57ac775
Gabriel Prochnau <2288@holbertonschool.com>
2020-10-23 13:37:11 +0000

    Add part 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums all arguments given to the function
+ *
+ * @n: ammount of parameters
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+va_list list;
+unsigned int i, sum = 0;
+va_start(list, n);
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+sum += va_arg(list, int);
+}
+}
+va_end(list);
+return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+Gabriel Prochnau`s directory for "Project: 0x10. C - Variadic functions" from Holberton School
