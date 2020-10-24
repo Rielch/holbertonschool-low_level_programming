451dddd165edb75a909893e5972302dc5b757e6c
Gabriel Prochnau <2288@holbertonschool.com>
2020-10-24 18:02:26 +0000

    Plis work

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -19,9 +19,9 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_int(char *str, va_arg list);
-void print_char(char *str, va_arg list);
-void print_float(char *str, va_arg list);
-void print_str(char *str, va_arg list);
+void print_int(char *str, va_list list);
+void print_char(char *str, va_list list);
+void print_float(char *str, va_list list);
+void print_str(char *str, va_list list);
 
 #endif /* VARIADIC_FUNCTIONS_H */
