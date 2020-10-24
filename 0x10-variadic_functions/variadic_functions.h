db2fafb55b8be518da390945345a902a1b381b9f
Gabriel Prochnau <2288@holbertonschool.com>
2020-10-24 17:51:16 +0000

    Fix part 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -19,9 +19,9 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_int(va_list list);
-void print_char(va_list list);
-void print_float(va_list list);
-void print_str(va_list list);
+void print_int(char *str, va_list list);
+void print_char(char *str, va_list list);
+void print_float(char *str, va_list list);
+void print_str(char *str, va_list list);
 
 #endif /* VARIADIC_FUNCTIONS_H */
