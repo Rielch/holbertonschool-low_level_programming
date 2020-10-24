34b5d1512de44f0f82781a025f6c42135213c164
Gabriel Prochnau <2288@holbertonschool.com>
2020-10-24 15:14:01 +0000

    Fix part 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,9 +15,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 va_list list;
 unsigned int i;
-va_start(list, n);
-if (n > 0 && list != NULL)
+if (n > 0)
 {
+va_start(list, n);
 for (i = 0; i < n; i++)
 {
 printf("%d", va_arg(list, int));
