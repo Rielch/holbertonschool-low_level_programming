2252a91fe102296401cf923fa605ca63e1abedd6
Gabriel Prochnau <2288@holbertonschool.com>
2020-10-30 02:05:26 +0000

    Add advanced 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * first - code to be executed before main
+ *
+ * Return: void
+ */
+
+__attribute__ ((__constructor__))
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
