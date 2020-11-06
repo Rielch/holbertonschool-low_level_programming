4aaacf5a41befc14eb17df40d34824a9e7c8fa6e
Gabriel Prochnau <2288@holbertonschool.com>
2020-11-06 00:12:12 +0000

    Add part 100

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * get_endianness - check the endianness of the system
+ *
+ * Return: 1 if little endian and 0 if big endian
+ */
+
+int get_endianness(void)
+{
+	char *str = "a";
+	if (&str[1] > &str[0])
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
