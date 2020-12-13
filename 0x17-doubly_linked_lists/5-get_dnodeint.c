badcee697566589a0a27afee5143a72d1cbd2bee
Gabriel Prochnau <2288@holbertonschool.com>
2020-12-13 19:02:53 +0000

    Fix part 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -13,6 +13,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *temp;
 	unsigned int n;
 
+	if (head == NULL)
+	{
+		return (NULL);
+	}
 	temp = head;
 	while (temp->prev != NULL)
 	{
