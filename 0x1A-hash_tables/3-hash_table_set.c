c77bb945e5c1d167521d209763645fedb40acd87
Gabriel Prochnau <2288@holbertonschool.com>
2021-01-15 19:48:17 +0000

    Fix part 3 3rd time

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,18 +12,17 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new, *temp;
-	unsigned long int index, size;
+	unsigned long int index;
 
-	if (ht == NULL || key == NULL || value == NULL || strcmp(key, "") == 0)
+	if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
 	{
 		return (0);
 	}
-	size = ht->size;
-	index = key_index((const unsigned char *) key, size);
+	index = key_index((const unsigned char *) key, ht->size);
 	temp = ht->array[index];
 	while (temp != NULL)
 	{
-		if (strcmp(temp->key, key))
+		if (strcmp(temp->key, key) == 0)
 		{
 			free(temp->value);
 			temp->value = strdup(value);
@@ -33,12 +32,18 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 	{
-		free(new);
 		return (0);
 	}
 	new->value = strdup(value);
 	new->key = strdup(key);
-	new->next = ht->array[index];
-	ht->array[index] = new;
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = new;
+	}
+	else
+	{
+		new->next = ht->array[index];
+		ht->array[index] = new;
+	}
 	return (1);
 }
